7434033e180d0c81b4ac11a852b184494fa1a23e
Cyril <984472100@qq.com>
2023-04-08 10:10:20 +0800

    框架及图片资源完成

---
+++ src/pages/myAttention/index.vue
@@ -0,0 +1,7 @@
+<template>
+	<div>我的关注</div>
+</template>
+
+<script></script>
+
+<style></style>
---
+++ src/pages/myCommunity/index.vue
@@ -0,0 +1,9 @@
+<template>
+	<div>我的社区</div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
